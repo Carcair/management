2d15f387c33c2f5778539c5c02c462dff9f253a4
Carcair <delic.emir90@gmail.com>
2020-09-21 13:08:59 +0200

    Minor fix

---
+++ config.js
@@ -0,0 +1,29 @@
+/**
+ * Secret variables for application
+ * if necessary transcrypt the file
+ */
+const config = {
+  // Environment
+  NODE_ENV: 'development',
+  // env: 'production',
+
+  // Port number
+  port: 5000,
+
+  // Base url
+  baseURL: 'http://localhost:5005',
+
+  // DB url
+  dbUrl: 'remotemysql.com',
+
+  // DB username
+  dbUsername: 'qJkXGOrBPl',
+
+  // DB name
+  dbName: 'qJkXGOrBPl',
+
+  // DB pass
+  dbPass: '45QgPdf7NP',
+};
+
+module.exports = config;
